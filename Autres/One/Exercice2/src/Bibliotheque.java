3f7ee93f27c1ff8be0edebde85db132f9000ecde
IkramKf <fessiikram1@gmail.com>
2022-12-08 10:43:23 +0100

    TP ajouté

--- Autres/One/Exercice2/src/Bibliotheque.java
+++ Autres/One/Exercice2/src/Bibliotheque.java
@@ -1,2 +1,56 @@
 public class Bibliotheque {
-}
+    int taille ;
+
+    private Livre b[] ;
+    public Bibliotheque(int taille){
+        b = new Livre[taille];
+    }
+    public void ajout ( Livre l , int nblivres  ){
+        if (nblivres+1<taille){
+            b[nblivres+1]=l;
+        }
+    }
+    public void supprimer(int r){
+        for ( int i=0 ;i<taille ;i++)
+            if(b[i].getRef()==r){
+                b[i]=b[i+1];
+                System.out.println("suppression effectue");
+
+            }else{
+                System.out.println("livre non disponible ");
+            }
+    }
+    public void afficher(){
+        for(int i=0 ; i<taille;i++){
+                b[i].afficher();
+            }
+        }
+
+
+    public void emprunter(String titre, String date){
+        for(int i =0 ; i<taille ; i++){
+            if(b[i].getTitre()==titre){
+                if(b[i].isEtat()==false){
+                    b[i].setEtat(true);
+                    b[i].setDate_emp(date);
+                    System.out.println("Le nouvel etat du livre est : "+b[i]);
+                }else{
+                    System.out.println("livre est deja emprunté ");
+                }
+            }else{
+                System.out.println("livre non dispo ");
+            }
+        }
+    }
+    public void Retour(String titre){
+        for(int i =0 ; i<taille ; i++){
+            if(b[i].getTitre()==titre){
+                b[i].setEtat(false);
+                b[i].setDate_emp("");
+            }else{
+                System.out.println("Livre introuvable");
+            }
+    }
+
+
+} }
--- Autres/One/Exercice2/src/Main.java
+++ Autres/One/Exercice2/src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Livre l1 = new Livre("12/12/2012", true, "Victor Hugo", "Causette", "ed1", 45);
+        Livre l2 = new Livre("10/10/2012", false, "Victor", "Dance", "ed3", 50);
+        Livre l3 = new Livre("09/04/2012", true, " Hugo", "LOVE", "ed2", 80);
+
+        Bibliotheque Bib = new Bibliotheque(5);
+        Bib.ajout(l1,0);
+        Bib.ajout(l2,1);
+        Bib.ajout(l3,2);
+        Bib.afficher();
+
+        Bib.supprimer(2);
+     
+        Bib.emprunter("LUNE","19/09/2015");
+        Bib.emprunter("Caussette","20/10/2018");
+
+        Bib.Retour("Causette");
+
+        Bib.afficher();
+
+
     }
 }
